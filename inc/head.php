c67eb70014e740ec04d9c20c2261268314c86b3c
cl33per <leeper08@gmail.com>
2019-09-07 12:09:44 -0400

    basic loging added, welcome page done, working on main page.

--- inc/head.php
+++ inc/head.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,4 +9,5 @@
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
     <title>Wiki</title>
 </head>
+
 <body>
